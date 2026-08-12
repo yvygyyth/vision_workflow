12ebc83d3a76c49455646cd4f27f1f84d9307076
yvygyyth <1589937631@qq.com>
2026-08-13 00:37:46 +0800

    1

--- src/vision_workflow/flows/parts/zhu_jiu_store/__init__.py
+++ src/vision_workflow/flows/parts/zhu_jiu_store/__init__.py
@@ -1,11 +1,15 @@
 """子流程：煮酒店铺。"""
 
 from vision_workflow.flows.parts.zhu_jiu_store.actions import (
-    click_buy1,
+    click_buy,
     click_entry,
     click_ming_jiang_ce,
     click_qing_mei_store,
     click_space_close,
+    click_ling_xi_box,
+    click_space_close2,
+    click_close,
+    click_return_btn,
 )
 from vision_workflow.module import END, MISS, OK, Flow, Module, abort, onward
 
@@ -19,8 +23,13 @@ FLOW = Flow(
         Module(id="entry", event=click_entry, on=_CLICK),
         Module(id="qing_mei_store", event=click_qing_mei_store, on=_CLICK),
         Module(id="ming_jiang_ce", event=click_ming_jiang_ce, on=_CLICK),
-        Module(id="buy1", event=click_buy1, on=_CLICK),
+        Module(id="buy", event=click_buy, on=_CLICK),
         Module(id="space_close", event=click_space_close, on=_CLICK),
+        Module(id="ling_xi-box", event=click_ling_xi_box, on=_CLICK),
+        Module(id="buy2", event=click_buy, on=_CLICK),
+        Module(id="space_close2", event=click_space_close2, on=_CLICK),
+        Module(id="close", event=click_close, on=_CLICK),
+        Module(id="return-btn", event=click_return_btn, on=_CLICK),
     ],
     success=END,
 )
--- src/vision_workflow/flows/parts/zhu_jiu_store/actions.py
+++ src/vision_workflow/flows/parts/zhu_jiu_store/actions.py
@@ -9,8 +9,13 @@ from vision_workflow.module import EventFn
 click_entry: EventFn = click("data/ming_jiang_sha/zhu_jiu_store/entry.png")
 click_qing_mei_store: EventFn = click("data/ming_jiang_sha/zhu_jiu_store/qing_mei-store.png")
 click_ming_jiang_ce: EventFn = click("data/ming_jiang_sha/common/ming_jiang_ce.png")
-click_buy1: EventFn = click("data/ming_jiang_sha/zhu_jiu_store/buy1.png")
+click_buy: EventFn = click("data/ming_jiang_sha/zhu_jiu_store/buy.png")
 click_space_close: EventFn = click(
-    "data/ming_jiang_sha/zhu_jiu_store/space-close.png",
-    offset_y=100,
+    "data/ming_jiang_sha/zhu_jiu_store/space-close.png"
 )
+click_ling_xi_box: EventFn = click("data/ming_jiang_sha/common/ling_xi-box.png")
+click_space_close2: EventFn = click(
+    "data/ming_jiang_sha/zhu_jiu_store/space-close2.png"
+)
+click_close: EventFn = click("data/ming_jiang_sha/common/store-close.png")
+click_return_btn: EventFn = click("data/ming_jiang_sha/common/return-btn.png")
