95ba635d2fcbc87fa24a172da7dec250a8188a3f
yvygyyth <1589937631@qq.com>
2026-08-19 20:56:42 +0800

    1

--- src/vision_workflow/apps/ming_jiang_sha/workflows/daily_free_resources/__init__.py
+++ src/vision_workflow/apps/ming_jiang_sha/workflows/daily_free_resources/__init__.py
@@ -1,5 +1,6 @@
 ﻿"""复杂流程：名将杀免费资源每日领取。"""
 
+from vision_workflow.apps.ming_jiang_sha.parts.Activity import FLOW as activity
 from vision_workflow.apps.ming_jiang_sha.parts.ba_qing_store import FLOW as ba_qing_store
 from vision_workflow.apps.ming_jiang_sha.parts.dang_qing_ge import FLOW as dang_qing_ge
 from vision_workflow.apps.ming_jiang_sha.parts.gong_hui import FLOW as gong_hui
@@ -11,7 +12,7 @@ from vision_workflow.module import FlowNode, Workflow
 WORKFLOW = Workflow(
     id="daily_free_resources",
     name="名将杀免费资源每日领取",
-    description="依次领取邮件、丹青阁、煮酒店铺、战役商店、巴清商店、公会店铺资源",
+    description="依次领取邮件、丹青阁、煮酒店铺、战役商店、巴清商店、活动、公会店铺资源",
     entry="mail",
     nodes=[
         FlowNode(mail),
@@ -19,6 +20,7 @@ WORKFLOW = Workflow(
         FlowNode(zhu_jiu_store),
         FlowNode(zhan_yi_store),
         FlowNode(ba_qing_store),
+        FlowNode(activity),
         FlowNode(gong_hui),
     ],
 )
--- src/vision_workflow/apps/ming_jiang_sha/workflows/solo/__init__.py
+++ src/vision_workflow/apps/ming_jiang_sha/workflows/solo/__init__.py
@@ -1,5 +1,6 @@
 """单流程调试用复杂流程：每个 Workflow 只包一个 Flow。"""
 
+from vision_workflow.apps.ming_jiang_sha.parts.Activity import FLOW as activity
 from vision_workflow.apps.ming_jiang_sha.parts.ba_qing_store import FLOW as ba_qing_store
 from vision_workflow.apps.ming_jiang_sha.parts.dang_qing_ge import FLOW as dang_qing_ge
 from vision_workflow.apps.ming_jiang_sha.parts.gong_hui import FLOW as gong_hui
@@ -31,6 +32,7 @@ WORKFLOWS: list[Workflow] = [
     _solo(zhu_jiu_store),
     _solo(zhan_yi_store),
     _solo(ba_qing_store),
+    _solo(activity),
     _solo(gong_hui),
     _solo(
         fight,
