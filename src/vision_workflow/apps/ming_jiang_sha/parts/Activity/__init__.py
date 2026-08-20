79a1716a6fc7a495018e84fea8d463bee46972a7
3282033378 <3282033378@qq.com>
2026-08-20 19:31:16 +0800

    1

--- src/vision_workflow/apps/ming_jiang_sha/parts/Activity/__init__.py
+++ src/vision_workflow/apps/ming_jiang_sha/parts/Activity/__init__.py
@@ -22,8 +22,8 @@ FLOW = Flow(
     entry="entry_icon",
     modules=[
         Module(id="entry_icon", name="打开活动", description="点击入口图标", event=click_huo_dong, on=_CLICK),
-        Module(id="bu_gua", name="移动鼠标至卜卦牌", description="移到 (1400,600)点击", event=move_aside, on=_CLICK,),
-        Module(id="bu_gua2", name="再次点击卜卦牌", description="再次点击", event=move_aside, config={"delay_ms": 500}, on=_CLICK),
+        Module(id="bu_gua", name="移动鼠标至卜卦牌", description="移到 (1400,600)点击", event=move_aside, on=_CLICK,config={"delay_ms": 1000}),
+        Module(id="bu_gua2", name="再次点击卜卦牌", description="再次点击", event=move_aside,  on=_CLICK),
         Module(id="space_close", name="关闭弹窗", description="Esc 关闭购买结果弹窗", event=space_close(), on=_OK),
         Module(id="gua_xiang", name="点击列表", description="点击列表", event=click_gua_xiang, on=_CLICK),
         Module(id="scroll", name="下滑列表", description="向下滚动列表", event=scroll_down, on=_OK),
